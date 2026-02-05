ec3b5c7928c0678933487d47bff690acfd1b9b10
syed <65139605+brainless3178@users.noreply.github.com>
2026-02-06 01:25:55 +0530

    [agent_a] Iteration 2: Update main_v2.rs (pure)

--- agents/agent_a/main_v2.rs
+++ agents/agent_a/main_v2.rs
@@ -1,61 +1,79 @@
-from solana.publickey import PublicKey
-from solana.system_program import TransferParams, transfer
-from solana.transaction import Transaction
 import solana
+from solana.publickey import PublicKey
+from spl.token.instructions import Transfer
+from spl.token.client import get_token_account
 
-# Constants
-AMM_POOL_ID = PublicKey("AMM_Pool_Public_Key")
-CONCENTRATED_LIQUIDITY_ID = PublicKey("Concentrated_Liquidity_Public_Key")
+# Define AMM pool class
+class AMM_Pool:
+    def __init__(self, token0, token1, liquidity):
+        self.token0 = token0
+        self.token1 = token1
+        self.liquidity = liquidity
 
-# Client setup
-client = solana.rpc.api.Client("https://api.devnet.solana.com")
+# Define CL pool class
+class CL_Pool:
+    def __init__(self, token0, token1, liquidity, tick_lower, tick_upper):
+        self.token0 = token0
+        self.token1 = token1
+        self.liquidity = liquidity
+        self.tick_lower = tick_lower
+        self.tick_upper = tick_upper
 
-def create_amm_pool(token_a, token_b, liquidity_provider):
-    """Create an AMM pool"""
-    transaction = Transaction()
-    transaction.add(transfer(liquidity_provider, AMM_POOL_ID, 1000000))
-    transaction.add(transfer(liquidity_provider, token_a, 1000000))
-    transaction.add(transfer(liquidity_provider, token_b, 1000000))
-    client.send_transaction(transaction)
+# Define function to create AMM pool
+def create_amm_pool(token0, token1, liquidity):
+    amm_pool = AMM_Pool(token0, token1, liquidity)
+    return amm_pool
 
-def add_liquidity(token_a, token_b, liquidity_provider):
-    """Add liquidity to an AMM pool"""
-    transaction = Transaction()
-    transaction.add(transfer(liquidity_provider, token_a, 1000000))
-    transaction.add(transfer(liquidity_provider, token_b, 1000000))
-    client.send_transaction(transaction)
+# Define function to create CL pool
+def create_cl_pool(token0, token1, liquidity, tick_lower, tick_upper):
+    cl_pool = CL_Pool(token0, token1, liquidity, tick_lower, tick_upper)
+    return cl_pool
 
-def create_concentrated_liquidity(token_a, token_b):
-    """Create concentrated liquidity"""
-    transaction = Transaction()
-    transaction.add(transfer(client.public_key, CONCENTRATED_LIQUIDITY_ID, 1000000))
-    client.send_transaction(transaction)
+# Define function to concentrate liquidity
+def concentrate_liquidity(amm_pools, cl_pools):
+    concentrated_liquidity = []
+    for pool in amm_pools + cl_pools:
+        concentrated_liquidity.append(pool.liquidity)
+    return concentrated_liquidity
 
-def execute_optimal_routing(token_a, token_b, liquidity_provider, amount):
-    """Execute optimal routing"""
-    # Calculate optimal route
-    optimal_route = calculate_optimal_route(token_a, token_b, amount)
+# Define function to transfer tokens
+def transfer_tokens(client, token_account, amount):
+    transfer_instruction = Transfer(
+        token_account,
+        amount,
+        [AccountMeta(token_account, False)]
+    )
+    return client.send_transaction(transfer_instruction, token_account)
+
+# Define function for optimal routing
+def optimal_routing(tokens, amounts):
+    # This function should implement the optimal routing algorithm
+    # For simplicity, it returns the first possible route
+    return [tokens[0], tokens[1]]
+
+# Define main function for Solana DEX
+def solana_dex():
+    # Initialize tokens and amounts
+    tokens = ["TOKEN1", "TOKEN2"]
+    amounts = [100, 200]
     
-    # Execute trades
-    for trade in optimal_route:
-        execute_trade(trade)
+    # Find optimal route using routing function
+    best_route = optimal_routing(tokens, amounts)
+    
+    # Create AMM and CL pools
+    amm_pool = create_amm_pool(tokens[0], tokens[1], amounts[0] * amounts[1])
+    cl_pool = create_cl_pool(tokens[0], tokens[1], amounts[0] * amounts[1], 100, 200)
+    
+    # Concentrate liquidity
+    concentrated_liquidity = concentrate_liquidity([amm_pool], [cl_pool])
+    
+    # Transfer tokens using concentrated liquidity
+    client = solana.Client()
+    for i in range(len(concentrated_liquidity)):
+        token_account = get_token_account(tokens[i])
+        amount = concentrated_liquidity[i]
+        transfer_instruction = transfer_tokens(client, token_account, amount)
+        print(f"Transaction {i+1} successful: {transfer_instruction}")
+        client.confirm_transaction(transfer_instruction)
 
-def calculate_optimal_route(token_a, token_b, amount):
-    """Calculate optimal route"""
-    # Implement optimal routing algorithm
-    pass
-
-def execute_trade(trade):
-    """Execute trade"""
-    # Implement trade execution
-    pass
-
-if __name__ == "__main__":
-    token_a = PublicKey("Token_A_Public_Key")
-    token_b = PublicKey("Token_B_Public_Key")
-    liquidity_provider = client.public_key
-
-    create_amm_pool(token_a, token_b, liquidity_provider)
-    add_liquidity(token_a, token_b, liquidity_provider)
-    create_concentrated_liquidity(token_a, token_b)
-    execute_optimal_routing(token_a, token_b, liquidity_provider, 1000000)
+solana_dex()
