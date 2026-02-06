d5f31d345dbc1151c3c35c4d344071f79086f887
syed <65139605+brainless3178@users.noreply.github.com>
2026-02-06 10:11:02 +0530

    [agent_a] Iteration 1: Update main_v1.rs (pure)

--- agents/agent_a/main_v1.rs
+++ agents/agent_a/main_v1.rs
@@ -1,1 +1,54 @@
-GitHub Models reasoning failed.
+import numpy as np
+
+class SolanaDEX:
+    def __init__(self):
+        self.amm_pools = {}
+
+    def add_liquidity(self, token_a, token_b, amount_a, amount_b):
+        if (token_a, token_b) not in self.amm_pools:
+            self.amm_pools[(token_a, token_b)] = {
+                "reserve_a": amount_a,
+                "reserve_b": amount_b,
+            }
+        else:
+            self.amm_pools[(token_a, token_b)]["reserve_a"] += amount_a
+            self.amm_pools[(token_a, token_b)]["reserve_b"] += amount_b
+
+    def get_optimal_route(self, token_in, token_out, amount_in):
+        optimal_route = []
+        best_rate = 0
+
+        for pool in self.amm_pools:
+            if pool[0] == token_in:
+                rate = self.get_rate(pool, amount_in)
+                if rate > best_rate:
+                    best_rate = rate
+                    optimal_route = [pool]
+
+        return optimal_route, best_rate
+
+    def get_rate(self, pool, amount_in):
+        reserve_in = self.amm_pools[pool]["reserve_a"]
+        reserve_out = self.amm_pools[pool]["reserve_b"]
+
+        return (reserve_out * amount_in) / (reserve_in + amount_in)
+
+    def execute_trade(self, token_in, token_out, amount_in):
+        optimal_route, best_rate = self.get_optimal_route(token_in, token_out, amount_in)
+
+        for pool in optimal_route:
+            self.amm_pools[pool]["reserve_a"] += amount_in
+            self.amm_pools[pool]["reserve_b"] -= best_rate * amount_in
+
+        return best_rate * amount_in
+
+
+# Create a new Solana DEX
+dex = SolanaDEX()
+
+# Add liquidity to the DEX
+dex.add_liquidity("USDT", "SOL", 1000, 100)
+
+# Execute a trade
+output_amount = dex.execute_trade("USDT", "SOL", 10)
+print(f"Output amount: {output_amount} SOL")
