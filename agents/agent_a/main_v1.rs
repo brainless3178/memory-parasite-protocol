d84434a97121713210827623f8cfd1e90f9bc589
syed <65139605+brainless3178@users.noreply.github.com>
2026-02-06 07:04:58 +0530

    [agent_a] Iteration 1: Update main_v1.rs (pure)

--- agents/agent_a/main_v1.rs
+++ agents/agent_a/main_v1.rs
@@ -1,41 +1,47 @@
 import numpy as np
+from solana.publickey import PublicKey
+from solana.rpc.api import Client
+
+# Constants
+ FeeTier = {
+    'LOW': 10,
+    'MEDIUM': 20,
+    'HIGH': 30
+}
 
 class SolanaDEX:
-    def __init__(self):
-        self.amm_pools = {}
-        self.concentrated_liquidity = {}
+    def __init__(self, rpc_url, fee_tier):
+        self.rpc_url = rpc_url
+        self.fee_tier = fee_tier
+        self.client = Client(self.rpc_url)
 
-    def add_amm_pool(self, token_pair, liquidity):
-        self.amm_pools[token_pair] = liquidity
+    def get_market(self, market_address):
+        market_pubkey = PublicKey(market_address)
+        return self.client.get_account_info(market_pubkey)
 
-    def add_concentrated_liquidity(self, token_pair, liquidity):
-        self.concentrated_liquidity[token_pair] = liquidity
+    def get_amm_pools(self, market_address):
+        market_pubkey = PublicKey(market_address)
+        amm_pools = []
+        for account in self.client.get_program_accounts(market_pubkey):
+            if account['account']['data']:
+                amm_pools.append(account['pubkey'])
+        return amm_pools
 
-    def optimal_routing(self, token_in, token_out, amount):
-        # Implement optimal routing algorithm
-        # For simplicity, assume we have a direct pool for the token pair
-        if (token_in, token_out) in self.amm_pools:
-            return self.amm_pools[(token_in, token_out)]
-        elif (token_out, token_in) in self.amm_pools:
-            return self.amm_pools[(token_out, token_in)]
-        else:
-            # Find the best route through multiple pools
-            best_route = None
-            best_rate = 0
-            for pool in self.amm_pools:
-                if token_in in pool:
-                    intermediate_token = pool[0] if pool[1] == token_in else pool[1]
-                    if (intermediate_token, token_out) in self.amm_pools:
-                        rate = self.amm_pools[pool] * self.amm_pools[(intermediate_token, token_out)]
-                        if rate > best_rate:
-                            best_rate = rate
-                            best_route = (pool, (intermediate_token, token_out))
-            return best_route
+    def get_concentrated_liquidity(self, amm_pool_address):
+        amm_pool_pubkey = PublicKey(amm_pool_address)
+        liquidity = self.client.get_account_info(amm_pubkey)
+        return liquidity
 
-    def get_liquidity(self, token_pair):
-        return self.concentrated_liquidity.get(token_pair, 0)
+    def optimize_routing(self, market_address, amm_pool_address):
+        market_info = self.get_market(market_address)
+        amm_pool_info = self.get_concentrated_liquidity(amm_pool_address)
+        # Calculate optimal routing based on market and pool data
+        optimal_routing = np.argmax([market_info, amm_pool_info])
+        return optimal_routing
 
-dex = SolanaDEX()
-dex.add_amm_pool(('USDT', 'SOL'), 1000)
-dex.add_concentrated_liquidity(('USDT', 'SOL'), 500)
-print(dex.optimal_routing('USDT', 'SOL', 100))
+# Usage
+dex = SolanaDEX('https://api.devnet.solana.com', FeeTier['MEDIUM'])
+market_address = '2p5gsC2j9F7zTivdCjPL5M9f7uTfNSu4hF4yZ5kXa2uq'
+amm_pool_address = '5T5x6sQ8pZ3JjE8pY7jL6L9pN1pM4pM4pM4pM4pM4p'
+optimal_routing = dex.optimize_routing(market_address, amm_pool_address)
+print(optimal_routing)
