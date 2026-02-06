cd0509b81ebbe554571e0ae126d92655037b72dd
syed <65139605+brainless3178@users.noreply.github.com>
2026-02-06 10:50:08 +0530

    [agent_a] Iteration 1: Update main_v1.rs (pure)

--- agents/agent_a/main_v1.rs
+++ agents/agent_a/main_v1.rs
@@ -1,43 +1,117 @@
 import pandas as pd
+from solana.publickey import PublicKey
+from solana.rpc.api import Client
 
-class SolanaDEX:
-    def __init__(self):
-        self.liquidity_pools = {}
-        self路由控制 = {}
+# Initialize Solana client
+client = Client("https://api.devnet.solana.com")
 
-    def create_pool(self, token_a, token_b):
-        self.liquidity_pools[(token_a, token_b)] = {'token_a': token_a, 'token_b': token_b, 'liquidity': 0}
+# Define serum DEX program ID
+DEX_PROGRAM_ID = PublicKey("9xQeWvG816bUx9EPjHmaT23yvVM2ZWbWVxeZZpL4A6K4")
 
-    def add_liquidity(self, token_a, token_b, amount_a, amount_b):
-        if (token_a, token_b) not in self.liquidity_pools:
-            self.create_pool(token_a, token_b)
-        self.liquidity_pools[(token_a, token_b)]['liquidity'] += amount_a * amount_b
+# Define token swap function
+def token_swap(
+    user_keypair,
+    user_token_account,
+    pool_token_account,
+    amount_in,
+    min_amount_out,
+):
+    # Create transaction
+    tx = client.transaction()
 
-    def optimal_routing(self, token_in, token_out, amount_in):
-        best_route = None
-        best_rate = 0
-        for token_a, token_b in self.liquidity_pools:
-            if token_a == token_in:
-                rate = self.liquidity_pools[(token_a, token_b)]['liquidity'] / (amount_in + self.liquidity_pools[(token_a, token_b)]['liquidity'])
-                if rate > best_rate and token_b == token_out:
-                    best_rate = rate
-                    best_route = (token_a, token_b)
-            elif token_b == token_in:
-                rate = self.liquidity_pools[(token_a, token_b)]['liquidity'] / (amount_in + self.liquidity_pools[(token_a, token_b)]['liquidity'])
-                if rate > best_rate and token_a == token_out:
-                    best_rate = rate
-                    best_route = (token_a, token_b)
-        return best_route
+    # Add token swap instruction
+    tx.add_instruction(
+        client.spl_token(
+            "transfer",
+            {
+                "source": user_token_account,
+                "destination": pool_token_account,
+                "amount": amount_in,
+            },
+        )
+    )
 
-    def execute_trade(self, token_in, token_out, amount_in):
-        best_route = self.optimal_routing(token_in, token_out, amount_in)
-        if best_route:
-            amount_out = self.liquidity_pools[best_route]['liquidity'] * amount_in / (amount_in + self.liquidity_pools[best_route]['liquidity'])
-            self.liquidity_pools[best_route]['liquidity'] += amount_in * amount_out
-            return amount_out
-        else:
-            return 0
+    # Add serum DEX instruction
+    tx.add_instruction(
+        client.serum_dex(
+            "swap",
+            {
+                "account_info": [
+                    {"account": user_keypair.public_key},
+                    {"account": user_token_account},
+                    {"account": pool_token_account},
+                ],
+                "params": {
+                    "amount_in": amount_in,
+                    "min_amount_out": min_amount_out,
+                },
+            },
+        )
+    )
 
-dex = SolanaDEX()
-dex.add_liquidity('USDT', 'SOL', 1000, 1000)
-print(dex.execute_trade('USDT', 'SOL', 100))
+    # Send transaction
+    result = client.send_transaction(tx, user_keypair)
+
+    return result
+
+# Define concentrated liquidity pool function
+def concentrated_liquidity_pool(
+    user_keypair,
+    token_a_account,
+    token_b_account,
+    liquidity_amount_a,
+    liquidity_amount_b,
+):
+    # Create transaction
+    tx = client.transaction()
+
+    # Add concentrated liquidity pool instruction
+    tx.add_instruction(
+        client.spl_token(
+            "transfer",
+            {
+                "source": token_a_account,
+                "destination": token_b_account,
+                "amount": liquidity_amount_a,
+            },
+        )
+    )
+
+    # Add concentrated liquidity pool instruction
+    tx.add_instruction(
+        client.spl_token(
+            "transfer",
+            {
+                "source": token_b_account,
+                "destination": token_a_account,
+                "amount": liquidity_amount_b,
+            },
+        )
+    )
+
+    # Send transaction
+    result = client.send_transaction(tx, user_keypair)
+
+    return result
+
+# Execute functions
+user_keypair = None  # Set user keypair
+user_token_account = None  # Set user token account
+pool_token_account = None  # Set pool token account
+amount_in = 100  # Set amount in
+min_amount_out = 90  # Set min amount out
+
+result = token_swap(user_keypair, user_token_account, pool_token_account, amount_in, min_amount_out)
+
+token_a_account = None  # Set token A account
+token_b_account = None  # Set token B account
+liquidity_amount_a = 100  # Set liquidity amount A
+liquidity_amount_b = 100  # Set liquidity amount B
+
+result = concentrated_liquidity_pool(
+    user_keypair,
+    token_a_account,
+    token_b_account,
+    liquidity_amount_a,
+    liquidity_amount_b,
+)
